d73e4201f8937be6248be4c467527c0df8c70c59
FEEZAKULSOOM <batoolfarid947@gmail.com>
2025-08-05 20:42:36 -0700

    changed

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function getRandomColor() {
     let letters = "0123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 163)];
     }
     return color;
 }
